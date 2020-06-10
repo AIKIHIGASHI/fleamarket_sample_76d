f7567696f29ac519dcdeeedba2c8521e8029daa7
o-taka-yuma <64815738+o-taka-yuma@users.noreply.github.com>
2020-06-10 15:11:46 +0900

    dwa

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_10_012059) do
+
+ActiveRecord::Schema.define(version: 2020_06_10_040833) do
+
 
   create_table "brands", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -48,7 +50,8 @@ ActiveRecord::Schema.define(version: 2020_06_10_012059) do
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "prefecture_id"
+
+    t.integer "prefecture_id", null: false
     t.index ["user_id"], name: "index_destinations_on_user_id"
   end
 
